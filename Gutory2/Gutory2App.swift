aeb7e213c474d6a61b6ca25fea5bc395a1f64596
Mac <lainemantei@gmail.com>
2025-11-15 20:55:40 -0700

    Initial Commit

---
+++ Gutory2/Gutory2App.swift
@@ -0,0 +1,17 @@
+//
+//  Gutory2App.swift
+//  Gutory2
+//
+//  Created by Mac Mantei on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct Gutory2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
